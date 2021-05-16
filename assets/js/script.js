e3e6c180547c91c15861be7017695dad0806e10e
Hamzah <hamzahullah@yahoo.com>
2021-05-16 16:20:52 -0400

    See Description for full push details
    Added logic for if all falses to recall and reset
    Added logic to populate array with values for returning a password meeting parameters of user
    Created function evenlydistributed() to return evenly distributed possibilities for each type of password parameter. Used count to ensure that every character the user selects appears in generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,27 +40,23 @@ function initialPrompts(numberChoices, password) {
     default:
       if (numberChoices == -1) {
         var output = confirm("Press 'OK' to Generate Password or 'Cancel' to leave Password Generator")
-        if (output) 
-        {
-          if (password.passwordLength == "Length not Selected") 
-          {
+        var isTrue = 0;
+        if (output) {
+          if (password.passwordLength == "Length not Selected") {
             alert("Cannot Generate password, please select a password length")
             return resetRecall()
           }
-          for (let i in password) 
-          {
-            if (!isNaN(password[i])) 
-              {
-                continue;
-              } 
-              else if(password[i] != true || password[i] != false)
-              {
-                alert("Cannot Generate Password, all parameters are required to be filled")
-                return resetRecall()
-              }
-           }
-           alert("Generating Password")
-           return generatePassword(password)
+          for (let i in password) {
+            if (!isNaN(password[i])) {
+              continue;
+            }
+            else if (password[i] != true && password[i] != false) {
+              alert("Cannot Generate Password, all parameters are required to be filled")
+              return resetRecall()
+            }
+          }
+          alert("Generating Password")
+          return generatePassword(password)
         } else {
           alert("Exiting Generator")
         }
@@ -184,22 +180,156 @@ function resetRecall() {
 }
 
 function generatePassword(password) {
-
+  var randomPassword = [{ character: "", key: 0 }]
+  var randomPasswordArray = [];
+  var randomPasswordKey = [];
+  var finalArray = []
+  var isTrue = 0
   for (let i in password) {
-    if(password[i] == true)
-    {
-      if(i == 1)
-      {
-        
+    if (password[i] == true)
+      isTrue++;
+  }
+  if (isTrue == 0) {
+    alert("All parameters cannot be false")
+    return resetRecall()
+  }
+  for (var l = 1; l <= password.passwordLength; l++) {
+    randomPasswordKey.push(l)
+  }
+  for (var j = 0; j < password.passwordLength; j++) {
+    if (password.hasUpperCase == true) {
+      randomPassword.push({ character: String.fromCharCode(getRandom(65, 90)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
+    }
+    if (password.hasLowerCase == true) {
+      randomPassword.push({ character: String.fromCharCode(getRandom(97, 122)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
+    }
+    if (password.hasNumbers == true) {
+      randomPassword.push({ character: String.fromCharCode(getRandom(48, 57)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
+    }
+    if (password.hasSpecial == true) {
+      var randomSpecial = getRandom(1, 4) % 5
+      if (randomSpecial == 1) {
+        randomPassword.push({ character: String.fromCharCode(getRandom(33, 47)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
+      } else if (randomSpecial == 2) {
+        randomPassword.push({ character: String.fromCharCode(getRandom(58, 64)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
+      } else if (randomSpecial == 3) {
+        randomPassword.push({ character: String.fromCharCode(getRandom(91, 96)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
+      } else if (randomSpecial == 4) {
+        randomPassword.push({ character: String.fromCharCode(getRandom(123, 126)), key: randomPasswordArray.push(getRandom(1, password.passwordLength)) })
       }
     }
-    console.log(password[i]);
+  }
+
+
+  evenlyDistributed(isTrue, randomPassword, finalArray)
+
+  console.log(finalArray.join(''))
+  console.log(finalArray.length)
+  console.log(password)
+  console.log(randomPassword)
+  console.log(randomPasswordArray)
+  console.log(randomPasswordKey)
+}
+
+function evenlyDistributed(possibilities, randomPassword, finalArray) {
+  var count1 = 0
+  var count2 = 0
+  var count3 = 0
+  var count4 = 0
+  if (possibilities == 1)
+    for (var i = 1; i < randomPassword.length; i++)
+      finalArray.push(randomPassword[i].character.toString())
+  if (possibilities == 2) {
+    for (var i = 1; i < randomPassword.length; i += possibilities) {
+      var x = getRandom(1, possibilities)
+      if (count1 == 0)
+        x = 1
+      if (count2 == 0)
+        x = 2
+  
+      while (count1 > count2 && x == 1)
+        x = getRandom(1, possibilities)
+      while (count2 > count1 && x == 2)
+        x = getRandom(1, possibilities)
+
+      if (x == 1) {
+        finalArray.push(randomPassword[i].character.toString())
+        count1++
+      } else {
+        finalArray.push(randomPassword[i + (possibilities - 1)].character.toString())
+        count2++
+      }
+    }
+  }
+  if (possibilities == 3) {
+    for (var i = 1; i < randomPassword.length; i += possibilities) {
+      var x = getRandom(1, possibilities)
+      if (count1 == 0)
+        x = 1
+      if (count2 == 0)
+        x = 2
+      if (count3 == 0)
+        x = 3
+
+      while (count1 > count2 && count1 > count3 && x == 1)
+        x = getRandom(1, possibilities)
+      while (count2 > count1 && count2 > count3 && x == 2)
+        x = getRandom(1, possibilities)
+      while (count3 > count1 && count3 > count2 && x == 3)
+        x = getRandom(1, possibilities)
+
+      if (x == 1) {
+        finalArray.push(randomPassword[i].character.toString())
+        count1++
+      } else if (x == 2) {
+        finalArray.push(randomPassword[i + (possibilities - 1)].character.toString())
+        count2++
+      } else if (x == 3) {
+        finalArray.push(randomPassword[i + (possibilities - 2)].character.toString())
+        count3++
+      }
+    }
+  }
+  if (possibilities == 4) {
+    for (var i = 1; i < randomPassword.length; i += possibilities) {
+      var x = getRandom(1, possibilities)
+      if (count1 == 0)
+        x = 1
+      if (count2 == 0)
+        x = 2
+      if (count3 == 0)
+        x = 3
+      if (count4 == 0)
+        x = 4
+
+      while (count1 > count2 && count1 > count3 && count1 > count4 && x == 1)
+        x = getRandom(1, possibilities)
+      while (count2 > count1 && count2 > count3 && count2 > count4 && x == 2)
+        x = getRandom(1, possibilities)
+      while (count3 > count1 && count3 > count2 && count3 > count4 && x == 3)
+        x = getRandom(1, possibilities)
+      while (count4 > count1 && count4 > count2 && count4 > count3 && x == 4)
+        x = getRandom(1, possibilities)
+
+      if (x == 1) {
+        finalArray.push(randomPassword[i].character.toString())
+        count1++
+      } else if (x == 2) {
+        finalArray.push(randomPassword[i + (possibilities - 1)].character.toString())
+        count2++
+      } else if (x == 3) {
+        finalArray.push(randomPassword[i + (possibilities - 2)].character.toString())
+        count3++
+      } else if (x == 4) {
+        finalArray.push(randomPassword[i + (possibilities - 3)].character.toString())
+        count4++
+      }
+    }
   }
 }
-function getRandom (min, max)
-    {
-    return Math.floor(Math.random()* (max - min)) + min;
-    }
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function parameterPrompts() {
   password = initializedParameters()
   parameterModification = prompt("Select which parameters to modify:" +
@@ -233,5 +363,4 @@ var generateBtn = document.querySelector("#generate");
 // }
 
 // Add event listener to generate button
-
 generateBtn.addEventListener("click", parameterPrompts);
